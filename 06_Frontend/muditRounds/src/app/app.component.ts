a4eff8b2e90f17ac1fd73dac4bf463ef121fa58a
muditmalviya <malviyamudit7@gmail.com>
2024-03-26 11:43:13 +0530

    Added the home page

--- 06_Frontend/muditRounds/src/app/app.component.ts
+++ 06_Frontend/muditRounds/src/app/app.component.ts
@@ -5,11 +5,15 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RaiseIssueComponent } from './raise-issue/raise-issue.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LoginComponent, SignupComponent, ProfileComponent, RaiseIssueComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent, SignupComponent, ProfileComponent,
+            RaiseIssueComponent, HttpClientModule, FormsModule, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 06_Frontend/muditRounds/src/app/login/login.component.ts
+++ 06_Frontend/muditRounds/src/app/login/login.component.ts
@@ -1,12 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  // imports: [HttpClient],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // username!: string;
+  // password!: string;
 
+  // constructor(private http: HttpClient) { }
+
+  // login() {
+  //   this.http.post('http://localhost:3000/auth/signin', {
+  //     username: this.username,
+  //     password: this.password
+  //   }).subscribe(response => {
+  //     // handle your response here
+  //     console.log(response);
+  //   });
+  // }
 }
